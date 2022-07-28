18cb0d98bf7cb5a43b6b6de618b1a5001d7f8f2f
E1Crisss <cristiangau@hotmail.com>
2022-07-27 21:31:23 -0300

    correjido el problema de filter - sort

--- main.js
+++ main.js
@@ -117,7 +117,7 @@ const $cards = document.querySelector(".cards"),
   $template = document.getElementById("template-card").content,
   $fragment = document.createDocumentFragment(),
   $products = document.getElementById("products"),
-  $mostrarFiltro = document.getElementById('mostrarFiltro'),
+  $mostrarFiltro = document.getElementById("mostrarFiltro"),
   $btnSP = document.getElementById("btnSP"),
   $btnR = document.getElementById("btnReset");
 
@@ -140,71 +140,97 @@ const listSelect = () => {
   let $selecProduct = document.getElementById("selecProduct");
   let searchType =
     $selecProduct.options[$selecProduct.selectedIndex].text.toLowerCase();
-    
-    while ($products.firstChild) {
-      $products.removeChild($products.firstChild);
-    }
-    
-    while ($mostrarFiltro.firstChild) {
-      $mostrarFiltro.removeChild($mostrarFiltro.firstChild);
-    }
-    
-    const mostrarFruits = fruits.filter((fruta) => fruta.type === searchType);
-    
-    if (mostrarFruits) {
-      mostrarFruits.forEach((item) => {
-      $template.querySelector("img").setAttribute("src", item.img);
-      $template.querySelector("img").setAttribute("alt", item.name);
-      $template.querySelector("img").setAttribute("width", "140px");
-      $template.querySelector("h3").textContent = item.name.toUpperCase();
-      let $clone = $template.cloneNode(true);
-      $fragment.appendChild($clone);
-    });
-    let $mFiltro = document.createElement('p');
-    $mFiltro.innerHTML = `por: ${searchType}`;
-    $mostrarFiltro.appendChild($mFiltro);
-    $products.appendChild($fragment);
-  }
-}
 
-
-
-const mostrarOrden = () => {
-  const $selecValue = document.getElementById('selecValue');
-  const ordenValue = $selecValue.options[$selecValue.selectedIndex].text.toLowerCase();
-  
   while ($products.firstChild) {
     $products.removeChild($products.firstChild);
   }
-  console.log(ordenValue, 'ordervalue')
 
-  fruits.sort((a,b) => {
-    return b.price -a.price
+  while ($mostrarFiltro.firstChild) {
+    $mostrarFiltro.removeChild($mostrarFiltro.firstChild);
+  }
+
+  // VALIDANDO EL SELECT
+  if (searchType === "tipo de fruta") {
+    mostrarProductos();
+  } else {
+    if (searchType === "mayor precio") {
+      mostrarMayor();
+    }
+    if (searchType === "menor precio") {
+      mostrarMenor();
+    } else {
+      const mostrarFruits = fruits.filter((fruta) => fruta.type === searchType);
+
+      if (mostrarFruits) {
+        mostrarFruits.forEach((item) => {
+          $template.querySelector("img").setAttribute("src", item.img);
+          $template.querySelector("img").setAttribute("alt", item.name);
+          $template.querySelector("img").setAttribute("width", "140px");
+          $template.querySelector("h3").textContent = item.name.toUpperCase();
+          $template.querySelector("p").textContent = item.type;
+          $template.querySelector("span").textContent = item.price;
+          let $clone = $template.cloneNode(true);
+          $fragment.appendChild($clone);
+        });
+        let $mFiltro = document.createElement("p");
+        $mFiltro.innerHTML = `por: ${searchType}`;
+        $mostrarFiltro.appendChild($mFiltro);
+        $products.appendChild($fragment);
+      }
+    }
+  }
+};
+
+const mostrarMayor = () => {
+  let newFruits = [...fruits];
+  newFruits.sort((a, b) => {
+    return b.price - a.price;
   });
-  // mostrarProductos()
-  console.log(fruits)
- const verMenor = fruits;
-  if (verMenor) {
-    verMenor.forEach((item) => {
+  const verMayor = newFruits;
+  if (verMayor) {
+    verMayor.forEach((item) => {
       $template.querySelector("img").setAttribute("src", item.img);
       $template.querySelector("img").setAttribute("alt", item.name);
       $template.querySelector("img").setAttribute("width", "140px");
       $template.querySelector("h3").textContent = item.name.toUpperCase();
-  
+
       $template.querySelector("p").textContent = item.type;
       $template.querySelector("span").textContent = item.price;
       let $clone = $template.cloneNode(true);
       $fragment.appendChild($clone);
     });
     $products.appendChild($fragment);
-  };
-    let $mFiltro = document.createElement('p');
-    // $mFiltro.innerHTML = `por: ${searchType}`;
+    let $mFiltro = document.createElement("p");
     $mostrarFiltro.appendChild($mFiltro);
     $products.appendChild($fragment);
-  };
+  }
+};
 
+const mostrarMenor = () => {
+  let newFruits = [...fruits];
+  newFruits.sort((a, b) => {
+    return a.price - b.price;
+  });
+  const verMenor = newFruits;
+  if (verMenor) {
+    verMenor.forEach((item) => {
+      $template.querySelector("img").setAttribute("src", item.img);
+      $template.querySelector("img").setAttribute("alt", item.name);
+      $template.querySelector("img").setAttribute("width", "140px");
+      $template.querySelector("h3").textContent = item.name.toUpperCase();
 
+      $template.querySelector("p").textContent = item.type;
+      $template.querySelector("span").textContent = item.price;
+      let $clone = $template.cloneNode(true);
+      $fragment.appendChild($clone);
+    });
+    $products.appendChild($fragment);
+    let $mFiltro = document.createElement("p");
+    $mFiltro.innerHTML = `por: menor precio`;
+    $mostrarFiltro.appendChild($mFiltro);
+    $products.appendChild($fragment);
+  }
+};
 
 const reset = () => {
   while ($products.firstChild) {
@@ -222,19 +248,12 @@ const reset = () => {
 // Button for Submit - Reset
 $btnSP.addEventListener("click", (e) => {
   e.preventDefault();
-
-  if(listSelect()){
-    listSelect()
-  }
-  // listSelect();
-  // mostrarOrden();
-  
-
+  listSelect();
 });
 
 $btnR.addEventListener("click", (e) => {
   reset();
-  console.log(fruits)
+  console.log(fruits);
 });
 // Button for Submit - Reset
 
