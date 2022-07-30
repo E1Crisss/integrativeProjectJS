0bea4d45bca6057d3db2bb848405e28d1c810908
E1Crisss <cristiangau@hotmail.com>
2022-07-29 22:04:51 -0300

    termine la validadaciones, pero falto usar localstorage

--- user.js
+++ user.js
@@ -20,51 +20,76 @@ const loginVer = () => {
 
 $login_btn.addEventListener("click", signupVer);
 $signup_btn.addEventListener("click", loginVer);
-/*
-const signupAlert = () => {
-  alert("¡Te has registrado con exito!");
-};
-const loginAlert = () => {
-  alert("Has iniciado sesion");
-}; */
 
-// $signup_btnS.addEventListener("click", signupAlert);
-// $login_btnS.addEventListener("click", loginAlert);
-
-let $nombre = document.forms["signupForm"]["nombre"],
-  $apellido = document.forms["signupForm"]["apellido"],
-  $email = document.forms["signupForm"]["email"],
-  $password = document.forms["signupForm"]["password"];
+const $nombre = document.getElementById("nombre"),
+  $apellido = document.getElementById("apellido"),
+  $email = document.getElementById("email"),
+  $password = document.getElementById("password"),
+  $form = document.getElementById("form");
 
 const $signupError = document.querySelector(".signupError");
 
-function validarsignup() {
-  if($nombre.value.length <= 2) {
-    $signupError.style.display = "block";
-    $nombre.style.border = "1px solid red";
-    $signupError.innerText = "Ingresa tu nombre";
-    $nombre.focus;
-    return false;
+const validaEmail = (email) => {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
+};
+
+
+const validarUser = () => {
+  const nombreValor = $nombre.value.trim();
+  const apellidoValor = $apellido.value.trim();
+  const emailValor = $email.value.trim();
+  const passValor = $password.value.trim();
+
+  if (!nombreValor || nombreValor.length <= 2) {
+    validaFalla($nombre, "Ingresa un Nombre valido, sin espacios");
+  } else {
+    validaOk($nombre);
+    console.log("entro bien");
   }
-  if($apellido.value.length <= 2) {
-    $signupError.style.display = "block";
-    $apellido.style.border = "1px solid red";
-    $signupError.innerText = "Ingresa tu apellido";
-    $apellido.focus;
-    return false;
-    }
-  if($email.value.length <= 8 ) {
-    $signupError.style.display = "block";
-    $email.style.border = "1px solid red";
-    $signupError.innerText = "Ingresa tu email";
-    $email.focus;
-    return false;
-    }
-  if($password.value.length <= 3) {
-    $signupError.style.display = "block";
-    $password.style.border = "1px solid red";
-    $signupError.innerText = "Ingresa tu password";
-    $password.focus;
-    return false;
-    }
-}
+
+  if (!apellidoValor || apellidoValor.length <= 2) {
+    validaFalla($apellido, "Ingresa un Apellido valido sin espacio");
+  } else {
+    validaOk($apellido);
+    console.log("entro bien");
+  }
+
+  if (!emailValor) {
+    validaFalla($email, "Campo incompleto");
+  } else if (!validaEmail(emailValor)) {
+    validaFalla($email, "El email es invalido");
+  } else {
+    validaOk($email);
+  }
+
+  const er = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,18}$/;
+  if (!passValor) {
+    validaFalla(pass);
+  } else if (passValor.length < 8) {
+    validaFalla($password, "Debe tener 8 caracteres cómo mínimo.");
+  } else if (!passValor.match(er)) {
+    validaFalla($password, "Debe tener al menos una may., una min. y un núm.");
+  } else {
+    validaOk($password);
+  }
+};
+
+const validaFalla = (input, mensaje) => {
+  $signupError.style.display = "block";
+  input.style.border = "1px solid red";
+  $signupError.innerHTML = mensaje;
+  formControl.focus;
+};
+
+const validaOk = (input) => {
+  $signupError.innerHTML = "";
+  $signupError.style.display = "none";
+  input.style.border = "1px solid var(--main-bg-color)";
+};
+
+$form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  validarUser();
+});
